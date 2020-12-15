93d8cdd175323b03f44b1e7e778fd0317a66bd95
Rakesh <rakesh.rakshit@kartbites.com>
2020-12-15 19:45:32 +0530

    code removed

--- hacker-rank/golang/checkIfOneEditAway/checkIfOneEditAway.go
+++ hacker-rank/golang/checkIfOneEditAway/checkIfOneEditAway.go
@@ -74,18 +74,3 @@ func main() {
 	fmt.Printf("\n%t \n", result)
 
 }
-
-func readLine(reader *bufio.Reader) string {
-	str, _, err := reader.ReadLine()
-	if err == io.EOF {
-		return ""
-	}
-
-	return strings.TrimRight(string(str), "\r\n")
-}
-
-func checkError(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
